a4b7c04c1ddeac3886ea7bc78091d2d0ec9f44ff
alexeybelyaevv <alex05lev08@gmail.com>
2022-09-19 14:17:34 +0200

    searchSSR

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,24 +1,19 @@
 import { useEffect, useState } from "react";
 
-const performSearch = async (query: string, setSearch: any) => {
-  const data = await fetch(
+const performSearch = (query: string) => {
+  return fetch(
     `https://hatsa.com/api/search/public/afiproducts/search/${query}?dedupe=true`
   ).then((response) => response.json());
-  setSearch(data.data);
 };
 
 export default function Search() {
   const [searchResults, setSearchResults] = useState<Array<any>>([]);
   const [text, setText] = useState<string>("");
 
-  const searchOnChange = (eventText: string) => {
-    performSearch(eventText, setSearchResults);
-    setText(eventText);
-  };
-
   useEffect(() => {
-    performSearch("", setSearchResults);
-  }, []);
+    if (!text) return;
+    performSearch(text).then((response) => setSearchResults(response.data));
+  }, [text]);
 
   console.log(searchResults);
 
@@ -30,7 +25,7 @@ export default function Search() {
       <input
         value={text}
         onChange={(e) => {
-          searchOnChange(e.target.value);
+          setText(e.target.value);
         }}
         id="searchQuery"
         type="text"
@@ -44,3 +39,53 @@ export default function Search() {
     </div>
   );
 }
+
+//  SSR VER
+
+// import React, { KeyboardEventHandler } from "react";
+// import { SearchResult } from "../types/types";
+// import { performSearch } from "../api/search";
+// import { useRouter } from "next/router";
+// export default function Search({ searchQuery, searchResults, errorMessage }) {
+//   const router = useRouter();
+//   return (
+//     <div className="p-4">
+//       {errorMessage && <h2>{errorMessage}</h2>}
+//       <label htmlFor="searchQuery" className="mr-4">
+//         Search for:
+//       </label>
+//       <input
+//         defaultValue={searchQuery}
+//         id="searchQuery"
+//         type="text"
+//         onKeyUp={(event: React.KeyboardEvent) => {
+//           if (event.key === "Enter") {
+//             let query = (event.target as HTMLInputElement).value;
+//             router.push(`?search=${query}`);
+//           }
+//         }}
+//         className="border"
+//       />
+//       <ul>
+//         {searchResults.map(({ id, product: { title } }) => (
+//           <li key={id}>{title}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export async function getServerSideProps(context) {
+//   const searchQuery = context.query.search;
+//   let result: SearchResult[] = [];
+//   let errorMessage = null;
+//   try {
+//     result = (await performSearch(searchQuery)).data ?? [];
+//   } catch (error) {
+//     errorMessage = "Opps!!! Something went wrong." + error;
+//   }
+
+//   return {
+//     props: { searchQuery, searchResults: result, errorMessage }, // will be passed to the page component as props
+//   };
+// }
